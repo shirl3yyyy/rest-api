aa19c6a7e654e68eba99e99088b89b203e4e065f
Shirley Oduor <shirleycaren254@gmail.com>
2025-09-10 17:23:44 -0700

    Fix server.js location and update env + User model

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+//schema for user
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true},
+    email: { type: String, required: true, unique: true},
+    age: { type: Number, required: true},
+});
+
+module.exports = mongoose.model('User', userSchema);
